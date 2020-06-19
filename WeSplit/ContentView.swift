93492b024312304431834051b5e1aa3f250f57cf
jackfullstack <ingurz@icloud.com>
2020-06-19 16:03:06 +0100

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ingrid van den Berg on 19/06/2020.
+//  Copyright © 2020 thumbzupio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
